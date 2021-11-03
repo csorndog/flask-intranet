73ba3f1d4a2d63118415b10bd6c43e706de699d6
csorndog <zcsorny@gmail.com>
2021-11-03 19:23:39 -0400

    Added flask-migrate to flaskLT to log all future database changes. Migrations folder is created from the flask db init command that I ran on the shell

--- flaskLT.py
+++ flaskLT.py
@@ -1,44 +1,12 @@
 ''' creating models/tables for Lifetime Prop. with Flask Book as reference example '''
 
 from flask import Flask
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
-
-"""
-def getSqlAlchemyString():
-        ''' Set configuration variables for MySQL Access via SQLAlchemy '''
-        # string parameters 
-        username = "flaskapp"
-        password = "lifetimeProperties123!"
-        host = "localhost"
-        db_name = "testdb"
-        db_cnctr_mod = "pymysql"   # using PyMySql but will change for other modules
-        
-        # optional configs
-        default_mysql_port = True
-        if not default_mysql_port:
-            port = input("Input non-default port #:  ")
-            uri_prefix = mysql+{db_cnctr_mod}://
-            uri_db_confs = 
-            sqlalch_uri = f"mysql+{pymysql_connector_module}://{username}:{password}@{host}/{db_name}"
-            mysql_uri_str = f"mysql+mysqlconnector://{username}:{password}@{host}/{db_name}"
-
-        port = "3306"     ## default mysql port
-        # sqlachemy uri variable
-        # mysql_uri_str = f"mysql+mysqlconnector://{username}:{password}@{host}/{db_name}"
-        mysql_uri_str = f"mysql+mysqlconnector://{username}:{password}@{host}/{db_name}"
-"""
-
 def getSqlAlchemyString():
         ''' Set configuration variables for MySQL Access via SQLAlchemy '''
 
-        # string parameters 
-        #username = "flaskapp"
-        #password = "flaskappTcp12!"
-        #host = "127.0.0.1"
-        #db_name = "testdb"
-        #db_cnctr_mod = "pymysql"   # using PyMySql but will change for other modules
-        
         # string parameters 
         username = "root"
         password = "ubu2Root!"
@@ -57,7 +25,7 @@ sqlalchemy_uri_str =  getSqlAlchemyString()
 app.config['SQLALCHEMY_DATABASE_URI']= sqlalchemy_uri_str
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
+migrate = Migrate(app, db)
 
 
 # creating role/user MODELS tables  (copied from book)
